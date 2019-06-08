c4fac8e4629d1ad5868570a60d398a3dfd3e7c6d
AbrahamBriones <abraham.briones1501@alumnos.ubiobio.cl>
2019-06-07 21:48:56 -0400

    Initial commit
    Primera Entrega
    
    Co-Authored-By: scep953d <scep953d@users.noreply.github.com>

---
+++ platforms/browser/www/js/my-app.js
@@ -0,0 +1,65 @@
+// Initialize app
+var myApp = new Framework7();
+
+// If we need to use custom DOM library, let's save it to $$ variable:
+var $$ = Dom7;
+// Funcion que permite definir el archivo raiz del proyecto y navegar por diferentes rutas que
+// sean establecidas
+var app = new Framework7({
+    // App root element
+    root: '#app',
+    // App Name
+    name: 'My App',
+    // App id
+    id: 'com.myapp.test',
+    // Enable swipe panel
+    panel: {
+      swipe: 'left',
+    },
+    // Se agregan las distintas rutas de archivos html externos
+    routes: [
+      {
+        path: '/about/',
+        url: 'about.html',
+      },
+      {
+        path: '/inicio/',
+        url: 'inicio.html',
+      },
+      {
+        path: '/notas/',
+        url: 'notas.html',
+      },
+      {
+        path: '/actividades/',
+        url: 'actividades.html',
+      },{
+        path: '/horario/',
+        url: 'horario.html',
+      },{
+        path: '/anotaciones/',
+        url: 'anotaciones.html',
+      },{
+        path: '/inasistencias/',
+        url: 'inasistencias.html',
+      },{
+        path: '/avisos/',
+        url: 'avisos.html',
+      },
+    ]
+    // ... other parameters
+  });
+//Función que carga una vista principal en el html raiz definido previamente
+var mainView = app.views.create('.view-main');
+
+// Handle Cordova Device Ready Event
+//Funcion que detecta cuando se inicia el proyecto en phonegap
+$$(document).on('deviceready', function() {
+    console.log("Device is ready!");
+});
+// Funcion que detecta cuando se inicia y carga una página
+// Option 1. Using one 'page:init' handler for all pages
+$$(document).on('page:init', function (e) {
+    // Do something here when page loaded and initialized
+    console.log(e);
+})
